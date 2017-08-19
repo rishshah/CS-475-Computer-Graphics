9fdd4e6a2cedcf7d08d5e67e0e85a3496c252207
Rishabh Shah <rishrocks17@gmail.com>
2017-08-19 18:25:53 +0530

    Grid structure in modelling

--- Assignment 1/model/model.hpp
+++ Assignment 1/model/model.hpp
@@ -13,29 +13,22 @@ public:
 };
 
 class Model{
-private:
-	
-	glm::vec3 centroid;
-	void calc_centroid();
-	
-
 public:
 	
 	float red_value = 1.0;
 	float green_value = 1.0;
 	float blue_value = 1.0;
+	
 	int num_of_vertices = 0;
 	std::vector<Vertex> vertex_list;
+	
 	int num_of_triangles = 0;
 	std::vector<int> configuration_list;
-	void combine_configuration_and_vertices();
 
 	std::vector<glm::vec3> position_ptr;
 	std::vector<glm::vec3> color_ptr;
 
-	int get_num_of_triangles();
-	glm::vec3 get_centroid();
-
+	void combine_configuration_and_vertices();
 	bool load(char* filename);
 	bool save(char* filename);
 	void assignBuffer(GLuint &vbo, GLuint &vao, GLuint &vPosition, GLuint &vColor);
