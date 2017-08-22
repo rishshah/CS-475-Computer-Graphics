21e321b1e81bf4aad0602f18c650298df88ff5c5
Purav <purav97@github.com>
2017-08-23 03:27:12 +0530

    model bat

--- Assignment 1/modelling/render.cpp
+++ Assignment 1/modelling/render.cpp
@@ -232,14 +232,14 @@ void handle_entry_mode() {
 
 void handle_io() {
     if (key_state_io[0]) {
-        m.save((char*)"./model/saved_model.raw");
-        printf("Model saved in saved_model.raw!\n");
+        m.save((char*)"./model/cricket_bat.raw");
+        printf("Model saved in cricket_bat.raw!\n");
         key_state_io[0] = false;
     }
     if (key_state_io[1]) {
-        m.load((char*)"./model/model.raw");
+        m.load((char*)"./model/cricket_bat.raw");
         initBuffersGL();
-        printf("Model loaded from model.raw!\n");
+        printf("Model loaded from cricket_bat.raw!\n");
         key_state_io[1] = false;
     }
 }
@@ -312,19 +312,20 @@ namespace modellingMode {
  *
  * @param      window  The window
  */
-void renderGL(GLFWwindow* window) {
-    handle_io();
-    handle_mouse_click(window);
-    handle_mouse_location(window);
-    handle_depth();
-    handle_fixed_rotation();
-    handle_color();
-    handle_entry_mode();
-    render_last_mouse_point();
-    modelview_matrix = ortho_projection_matrix * translation_matrix * rotation_matrix;
-    glBindVertexArray(vao);
-    glUniformMatrix4fv(uModelViewMatrix, 1, GL_FALSE, glm::value_ptr(modelview_matrix));
-    glDrawArrays(GL_TRIANGLES, 0, m.num_of_triangles * 3);
-}
+		
+	void renderGL(GLFWwindow* window) {
+	    handle_io();
+	    handle_mouse_click(window);
+	    handle_mouse_location(window);
+	    handle_depth();
+	    handle_fixed_rotation();
+	    handle_color();
+	    handle_entry_mode();
+	    render_last_mouse_point();
+	    modelview_matrix = ortho_projection_matrix * translation_matrix * rotation_matrix;
+	    glBindVertexArray(vao);
+	    glUniformMatrix4fv(uModelViewMatrix, 1, GL_FALSE, glm::value_ptr(modelview_matrix));
+	    glDrawArrays(GL_TRIANGLES, 0, m.num_of_triangles * 3);
+	}
 };
 
