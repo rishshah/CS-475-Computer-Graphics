425c3d45bf427cdaa928f56aa828020b414293d1
Purav <purav97@github.com>
2017-11-26 22:25:03 +0530

    sky, spaceship, light position

--- Assignment 4/shaders/fshader.glsl
+++ Assignment 4/shaders/fshader.glsl
@@ -21,7 +21,7 @@ void main ()
 
   vec4 ori_position = mult_matrix * position_pt ;
 
-  vec3 spot_light_pos = vec3(0.0, 8.0, 0.0);
+  vec3 spot_light_pos = vec3(0.0, 15.0, 0.0);
   
   vec3 spot_light_dir = vec3(0.0, -1.0, 0.0);
 
@@ -36,14 +36,14 @@ void main ()
   	vec3 temp_vec3 = vec3(min(tex_color.x,1.0), min(tex_color.y,1.0), min(tex_color.z,1.0));
   	vec4 new_color = vec4(min((temp_vec3*light_inten + vec3(spec_shine, spec_shine, spec_shine)), vec3(1.0)), 1.0);
   	
-    if(light_present == 1 && val > spot_cutoff && ori_position.y > 0)
+    if(light_present == 1 && val > spot_cutoff && ori_position.y > 0 && ori_position.y < spot_light_pos.y)
   		frag_color = vec4(new_color.x+0.2, new_color.y+0.2, new_color.z+0.2, color.w);
   	else
   		frag_color = new_color;
 
   } else {
   
-  	if(light_present == 1 && val > spot_cutoff && ori_position.y > 0)
+  	if(light_present == 1 && val > spot_cutoff && ori_position.y > 0 && ori_position.y < spot_light_pos.y)
   		frag_color = vec4(color.x+0.2, color.y+0.2, color.z+0.2, color.w);
   	else
   		frag_color = color;
